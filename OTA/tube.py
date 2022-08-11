56f04a4a815869e0641fecc9a34943fe73fa133b
TA2KVC <62475996+TA2KVC@users.noreply.github.com>
2022-08-11 04:07:56 +0300

    Update tube.py

--- OTA/tube.py
+++ OTA/tube.py
@@ -1,5 +1,5 @@
-{"https://youtu.be/vYuhA1rQkaM"} = "volk1"
-{"https://youtu.be/uSiHqxgE2d0"} = "volk2"
-{"https://youtu.be/otCpCn0l4Wo"} = "volk3"
-{"https://youtu.be/djV11Xbc914"} = "volk4"
-{"https://youtu.be/OMOGaugKpzs"} = "volk5"
+{"https://youtu.be/vYuhA1rQkaM" = "volk1"}
+{"https://youtu.be/uSiHqxgE2d0" = "volk2"}
+{"https://youtu.be/otCpCn0l4Wo" = "volk3"}
+{"https://youtu.be/djV11Xbc914" = "volk4"}
+{"https://youtu.be/OMOGaugKpzs" = "volk5"}
