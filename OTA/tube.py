ec37d19e3845e0dfe8cdbc6b04f523ec6fffcb9c
TA2KVC <62475996+TA2KVC@users.noreply.github.com>
2022-08-13 00:49:50 +0300

    Update tube.py

--- OTA/tube.py
+++ OTA/tube.py
@@ -1,70 +1,52 @@
-https://youtu.be/vYuhA1rQkaM Joe Cocker - Unchain My Heart
-https://youtu.be/uSiHqxgE2d0 Ray Charles - Hit The Road Jack
-https://youtu.be/otCpCn0l4Wo M.C. Hammer - U Can't Touch This
-https://youtu.be/djV11Xbc914 a-ha - Take On Me
-https://youtu.be/OMOGaugKpzs The Police - Every Breath You Take
-https://youtu.be/n4RjJKxsamQ Scorpions - Wind Of Change
-https://youtu.be/fJ9rUzIMcZQ Queen – Bohemian Rhapsody
-https://youtu.be/Zi_XLOBDo_Y Michael Jackson - Billie Jean
-https://youtu.be/9jK-NcRmVcw Europe - The Final Countdown
-https://youtu.be/Rbm6GXllBiw Guns N' Roses - Paradise City
-https://youtu.be/xwtdhWltSIg R.E.M. - Losing My Religion
-https://youtu.be/oRdxUFDoQe0 Michael Jackson - Beat It
-https://youtu.be/v2AC41dglnM AC/DC - Thunderstruck
-https://youtu.be/dsUXAEzaC3Q Michael Jackson - Bad
-https://youtu.be/HzZ_urpj4As Michael Jackson - The Way You Make Me
-https://youtu.be/pAgnJDJN4VA AC/DC - Back In Black
-https://youtu.be/YR5ApYxkU-U Pink Floyd - Another Brick In The Wall
-https://youtu.be/eM8Ss28zjcE Queen - I Want To Break Free
-https://youtu.be/Qt2mbGP6vFI Phil Collins - Another Day In Paradise
-https://youtu.be/t1TcDHrkQYg Alphaville - Forever Young
-https://youtu.be/loWXMtjUZWM Foreigner - I want to
-https://youtu.be/izGwDsrQ1eQ George Michael - Careless Whisper
-https://youtu.be/FTQbiNvZqaY Toto - Africa
-https://youtu.be/KrZHPOeOxQQ Bon Jovi - You Give Love A Bad Name
-https://youtu.be/rY0WxgSXdEE Queen - Another One Bites the Dust
-https://youtu.be/pIgZ7gMze7A Wham! - Wake Me Up
-https://youtu.be/qeMFqkcPYcg Eurythmics - Sweet Dreams
-https://youtu.be/eH3giaIzONA Whitney Houston - I Wanna Dance
-https://youtu.be/GOJk0HW_hJw Whitesnake - Is This Love
-https://youtu.be/WGU_4-5RaxU Blondie - Heart Of Glass
-https://youtu.be/9f06QZCVUHg Bryan Adams - Summer Of’69
-https://youtu.be/CdqoNKCCt7A Simple Minds - Don't You
-https://youtu.be/hCuMWrfXG4E Billy Joel - Uptown Girl
-https://youtu.be/J9gKyRmic20 Crowded House - Don't Dream
-https://youtu.be/fPO76Jlnz6c Coolio - Gangsta's Paradise
-https://youtu.be/hTWKbfoikeg Nirvana - Smells Like Teen Spirit
-https://youtu.be/Y0pdQU87dc8 Bryan Adams - Everything I Do
-https://youtu.be/6Ejga4kJUts The Cranberries - Zombie
-https://youtu.be/tAGnKpE4NCI Metallica: Nothing Else Matters
-https://youtu.be/1lyu1KKwC74 The Verve - Bitter Sweet Symphony
-https://youtu.be/QNJL6nfu__Q Michael Jackson - They Don’t Care
-https://youtu.be/NKMtZm2YuBE Puff Daddy - I'll Be Missing You
-https://youtu.be/CD-E-LDc384 Metallica: Enter Sandman
-https://youtu.be/ZJL4UGSbeFg Shania Twain - I Feel Like A Woman
-https://youtu.be/mzJj5-lubeM Red Hot Chili Peppers - Scar Tissue
-https://youtu.be/YP3W-E0OamU Michael Jackson - Black or White
-https://youtu.be/gJLIiF15wjQ Spice Girls - Wannabe
-https://youtu.be/C3lWwBslWqg Sting - Desert Rose
-https://youtu.be/mOYZaiDZ7BM Rednex - Cotton Eye Joe 
-https://youtu.be/d73tiBBzvFM Ace of Base - All That She Wants
-https://youtu.be/YlUKcNNmywk Red Hot Chili Peppers - Californication
-https://youtu.be/PCTI_e81DtM The Prodigy - Smack My Bitch Up
-https://youtu.be/nllmm7Q4sj0 The Prodigy - Breathe
-https://youtu.be/uCM_4J6xdKg The Prodigy - Diesel Power
-https://youtu.be/psCf0oK9dvg The Prodigy - Funky Shit
-https://youtu.be/76ZRlYlyVu0 The Prodigy - Serial Thrilla
-https://youtu.be/rMbyJnLOhCM The Prodigy - Mindfiels
-https://youtu.be/iU5UKGTk-XI The Prodigy - Narayan
-https://youtu.be/_SUY0ujOenU The Prodigy - Firestarter
-https://youtu.be/HuZTEsvgQko The Prodigy - Climbatize
-https://youtu.be/sf3QD3VoOnU The Prodigy - Fuel My Fire
-https://youtu.be/c18441Eh_WE Kylie Minogue - Can't Get You
-https://youtu.be/63vqob-MljQ Kylie Minogue - Come Into My World
-https://youtu.be/vx2u5uUu3DE Bon Jovi - It's My Life
-https://youtu.be/d27gTrPPAyk Sting - Englishman In New York
-https://youtu.be/mqFLXayD6e8 Shania Twain - That Don't Impress Me Much
-https://youtu.be/llyiQ4I-mcQ Vengaboys - Boom, Boom, Boom!!
+https://youtu.be/loWXMtjUZWM
+https://youtu.be/izGwDsrQ1eQ
+https://youtu.be/FTQbiNvZqaY
+https://youtu.be/KrZHPOeOxQQ
+https://youtu.be/rY0WxgSXdEE
+https://youtu.be/pIgZ7gMze7A
+https://youtu.be/qeMFqkcPYcg
+https://youtu.be/eH3giaIzONA
+https://youtu.be/GOJk0HW_hJw
+https://youtu.be/WGU_4-5RaxU
+https://youtu.be/9f06QZCVUHg
+https://youtu.be/CdqoNKCCt7A
+https://youtu.be/hCuMWrfXG4E
+https://youtu.be/J9gKyRmic20
+https://youtu.be/fPO76Jlnz6c
+https://youtu.be/hTWKbfoikeg
+https://youtu.be/Y0pdQU87dc8
+https://youtu.be/6Ejga4kJUts
+https://youtu.be/tAGnKpE4NCI
+https://youtu.be/1lyu1KKwC74
+https://youtu.be/QNJL6nfu__Q
+https://youtu.be/NKMtZm2YuBE
+https://youtu.be/CD-E-LDc384
+https://youtu.be/EK_LN3XEcnw
+https://youtu.be/ZJL4UGSbeFg
+https://youtu.be/EScLmWJs82I
+https://youtu.be/mzJj5-lubeM
+https://youtu.be/YP3W-E0OamU
+https://youtu.be/gJLIiF15wjQ
+https://youtu.be/C3lWwBslWqg
+https://youtu.be/mOYZaiDZ7BM
+https://youtu.be/d73tiBBzvFM
+https://youtu.be/YlUKcNNmywk
+https://youtu.be/PCTI_e81DtM
+https://youtu.be/nllmm7Q4sj0
+https://youtu.be/uCM_4J6xdKg
+https://youtu.be/psCf0oK9dvg
+https://youtu.be/76ZRlYlyVu0
+https://youtu.be/rMbyJnLOhCM
+https://youtu.be/iU5UKGTk-XI
+https://youtu.be/_SUY0ujOenU
+https://youtu.be/HuZTEsvgQko
+https://youtu.be/sf3QD3VoOnU
+https://youtu.be/c18441Eh_WE
+https://youtu.be/63vqob-MljQ
+https://youtu.be/vx2u5uUu3DE
+https://youtu.be/d27gTrPPAyk
+https://youtu.be/mqFLXayD6e8
+https://youtu.be/llyiQ4I-mcQ
 https://youtu.be/HCm6gRHINqA
 https://youtu.be/ymNFyxvIdaM
 https://youtu.be/NI6aOFI7hms
