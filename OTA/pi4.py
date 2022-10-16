5e8aeaa5bb7b75c8bd0d630ab4a8119b00a4e2f8
TA2KVC <62475996+TA2KVC@users.noreply.github.com>
2022-10-16 22:29:31 +0300

    Update pi4.py

--- OTA/pi4.py
+++ OTA/pi4.py
@@ -1,3 +1,9 @@
+https://i.ibb.co/tCsYynC/thibault-valsin-render-raspberry-pi4-v2-2.jpg
+https://i.ibb.co/fdyKFmn/pexels-craig-dennis-57007.jpg
 https://i.ibb.co/c8GYS4Q/New-8-GB-Raspberry-Pi-pivietnam-com-vn.png
 https://i.ibb.co/Q8Q3d4m/IMG-20221001-221746.jpg
 https://i.ibb.co/64JZ4Rn/RPi-family-GIF.gif
+https://i.ibb.co/zFcVCm5/4297-02.jpg
+https://i.ibb.co/PCgjjCm/pi-plug-in.gif
+https://i.ibb.co/PNHbcRJ/Raspberry-Pi-4-Model-B.jpg
+https://i.ibb.co/HqJhkxH/raspberry-pi-gpio-tutorial-1.gif
