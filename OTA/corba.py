8b2aec4dba410bcdd24e123b55932d4bb3236a95
TA2KVC <62475996+TA2KVC@users.noreply.github.com>
2023-06-08 20:22:27 +0300

    Create corba.py

---
+++ OTA/corba.py
@@ -0,0 +1,55 @@
+Mercimek Çorbası
+Tarhana Çorbası
+Domates Çorbası
+Ezogelin Çorbası
+Yoğurt Çorbası
+Tavuk Çorbası
+Sebze Çorbası
+Şehriye Çorbası
+Mantı Çorbası
+İşkembe Çorbası
+Bulgur Çorbası
+Patates Çorbası
+Lahana Çorbası
+Kremalı Mantar Çorbası
+Balık Çorbası
+Brokoli Çorbası
+Fasulye Çorbası
+Kereviz Çorbası
+Mısır Çorbası
+Kırmızı Mercimek Çorbası
+Bezelye Çorbası
+Nohut Çorbası
+Bamya Çorbası
+Pazı Çorbası
+Kabak Çorbası
+Pırasa Çorbası
+İspanak Çorbası
+Soğan Çorbası
+Yeşil Mercimek Çorbası
+Terbiyeli Tavuk Çorbası
+Mantarlı Tavuk Çorbası
+Mısır Unu Çorbası
+Taze Fasulye Çorbası
+Mantar Çorbası
+Yoğurtlu Yayla Çorbası
+Şehriyeli Tavuk Çorbası
+Patates Püresi Çorbası
+Kıymalı Nohut Çorbası
+Terbiyeli Mercimek Çorbası
+Şehriyeli Domates Çorbası
+Manti Çorbası
+Domatesli Tavuk Çorbası
+Şehriyeli Sebze Çorbası
+Mantarlı Yoğurt Çorbası
+Şehriyeli İşkembe Çorbası
+Şehriyeli Bulgur Çorbası
+Yoğurtlu Patates Çorbası
+Mısır Unlu Tavuk Çorbası
+Patatesli Mantar Çorbası
+Mantarlı Bezelye Çorbası
+Ezogelin Çorbası
+Yayla Çorbası
+Sütlü Domates Çorbası
+Brokoli Çorbası
+Hanımağa Çorbası
