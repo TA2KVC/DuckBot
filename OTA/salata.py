0a09d1c3f747ccdcd2ba2c758c622e5d1d46352c
TA2KVC <62475996+TA2KVC@users.noreply.github.com>
2023-06-08 20:23:44 +0300

    Create salata.py

---
+++ OTA/salata.py
@@ -0,0 +1,50 @@
+Yeşil Salata
+Çoban Salatası
+Mevsim Salatası
+Tavuklu Salata
+Akdeniz Salatası
+Semizotu Salatası
+Patates Salatası
+Rus Salatası
+Ton Balıklı Salata
+Nohut Salatası
+Kuskus Salatası
+Mercimek Salatası
+Pancar Salatası
+Zeytin Salatası
+Brokoli Salatası
+Kinoa Salatası
+Mantar Salatası
+Cevizli Roka Salatası
+Enginar Salatası
+Avokado Salatası
+Narlı Semizotu Salatası
+Havuç Salatası
+Marul Salatası
+Cevizli Lahana Salatası
+Mango Salatası
+Kereviz Salatası
+Antep Ezmesi Salatası
+Brüksel Lahanası Salatası
+Karışık Yeşillik Salatası
+Arugula Salatası
+Barbunya Salatası
+Roka Salatası
+Patlıcan Salatası
+Cevizli Pancar Salatası
+Zeytinyağlı Enginar Salatası
+Brokoli Kinoa Salatası
+Nohutlu Semizotu Salatası
+Ton Balıklı Mercimek Salatası
+Akdeniz Enginar Salatası
+Yeşil Mercimek Salatası
+Kırmızı Lahana Salatası
+Kuskuslu Semizotu Salatası
+Mantarlı Roka Salatası
+Mango Cevizli Marul Salatası
+Avokado Yoğurtlu Semizotu Salatası
+Brüksel Lahanası Portakallı Salata
+Kerevizli Havuç Salatası
+Barbunya Nohutlu Yeşil Salata
+Roka Arugula Karışık Yeşillik Salatası
+Patlıcan Kırmızı Biber Salatası
