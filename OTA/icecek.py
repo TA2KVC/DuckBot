0fcc1c2104d6799fd2a070eeddaf81ca09e19651
TA2KVC <62475996+TA2KVC@users.noreply.github.com>
2023-06-08 20:30:54 +0300

    Create icecek.py

---
+++ OTA/icecek.py
@@ -0,0 +1,26 @@
+Su
+Limonata
+Ayran
+Gazlı İçecek
+Meyve Suyu
+Portakal Suyu
+Elma Suyu
+Nar Suyu
+Havuç Suyu
+Soğuk Çay
+Yeşil Çay
+Siyah çay
+Şerbet
+Vişne Şerbeti
+Nar Şerbeti
+Gül Şerbeti
+Bitki Çayı
+Smoothie
+Bira
+Şarap
+Beyaz Şarap
+Kırmızı Şarap
+Rose Şarap
+Acılı Şalgam
+Şalgam
+
