f7986af54ffb8a0dac858a63121611e388fecf43
TA2KVC <62475996+TA2KVC@users.noreply.github.com>
2023-06-08 19:49:33 +0300

    Create anayemek.py

---
+++ OTA/anayemek.py
@@ -0,0 +1,117 @@
+Püreli Tepsi Kebabı
+Fırında Kabak Mücver
+Fırında Kıymalı Patlıcan
+Beşamel Soslu Patlıcan Musakka
+Karnabahar Graten
+Patatesli Havuçlu Bezelye
+Fırında Patlıcan Graten
+Pırasa
+Kabak Mücver
+Kuru Fasulye
+Nohut Yemeği
+Patlıcan Oturtma
+Patlıcan Musakka
+Nohutlu Patlıcan Yemeği
+Patatesli Mantar Sote
+Dövme Aşür
+Bulgurlu Semizotu
+Bulgurlu Ispanak Yemeği
+Beğendili Mantar Dolması
+Patlıcan Yemeği
+Kıymalı Taze Kabak
+Tavuklu Bezelye
+İçli Köfte
+İmam Bayıldı
+Etli Kuru Fasulye
+Kuru Fasulye Pilav
+Türlü
+Karnıyarık
+Mantı
+İç Pilav
+Etli Patates Yemeği
+Lahmacun
+Etli Nohut
+Güveç
+Pırasa Yemeği
+Kuzu Tandır
+Kabak Dolması
+Tavuklu Pilav
+Fırında Tavuk
+Etli Bezelye
+Mantar Sote
+Zeytinyağlı Enginar
+Et Sote
+Kıymalı Patates Yemeği
+Yoğurtlu Kereviz
+Etli Bamya
+Tavuklu Nohut
+Sebzeli Bulgur Pilavı
+Kıymalı Ispanak
+Tavuklu Sebzeli Makarna
+Mantarlı Pilav
+Kıymalı Karnabahar
+Kereviz Dolması
+Yoğurtlu Kabak
+Etli Kuru Patlıcan
+Tavuklu Bezelye
+Ispanak Çorbası
+Kıymalı Pırasa
+Sebzeli Et Sote
+Fırında Patates Yemeği
+Kıymalı Kabak Dolması
+Tavuklu Ispanak
+Yoğurtlu Bezelye
+Etli Kereviz
+Tavuklu Patates Yemeği
+Yoğurtlu Karnıyarık
+Tavuklu Pırasa
+Nohutlu Bulgur Pilavı
+Mantarlı Tavuk Sote
+Kıymalı Biber Dolması
+Kereviz Çorbası
+Yoğurtlu Patates Yemeği
+Etli Enginar
+Tavuklu Kabak Dolması
+Kuru Patlıcan Dolması
+Tavuklu Ispanaklı Makarna
+Yoğurtlu Kuru Fasulye
+Kıymalı Kereviz
+Sebzeli Etli Pilav
+Ispanaklı Bulgur Pilavı
+Yoğurtlu Karnabahar
+Tavuklu Pırasalı Pilav
+Kıymalı Nohut
+Kabak Kızartması
+Etli Taze Fasulye
+Ispanak Sote
+Yoğurtlu Taze Fasulye
+Sebzeli Et Sote
+Tavuklu Kereviz
+Bezelye Yemeği
+Kıymalı Kabak Dolması
+Tavuklu Enginar
+Kereviz Çorbası
+Yoğurtlu Bezelye
+Etli Taze Fasulye
+Tavuklu Ispanak
+Mantarlı Tavuk Sote
+Kıymalı Pırasa
+Kabak Dolması
+Tavuklu Patates Yemeği
+Taze Fasulye
+Ispanak Çorbası
+Kıymalı Nohut
+Bezelye Pilavı
+Yoğurtlu Kuru Fasulye
+Kıymalı Enginar
+Tavuklu Kereviz
+Fırında Patates Kızartması
+Etli Taze Fasulye
+Ispanak Sote
+Yoğurtlu Taze Fasulye
+Sebzeli Et Sote
+Kıymalı Bezelye
+Tavuklu Karnabahar
+Tavuklu Pilav
+Fırında Sebzeli Tavuk
+Fırında Yeşil Kabak Mücveri
