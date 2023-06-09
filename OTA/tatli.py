c37a823170ab28ce3c356e6306456f0805654eca
TA2KVC <62475996+TA2KVC@users.noreply.github.com>
2023-06-09 20:18:36 +0300

    Create tatli.py

---
+++ OTA/tatli.py
@@ -0,0 +1,69 @@
+Baklava
+Künefe
+Şekerpare
+Sütlaç
+Güllaç
+Revani
+Aşure
+İrmik Helvası
+Kadayıf
+Kazandibi
+Muhallebi
+Dondurma
+Kabak Tatlısı
+Tulumba Tatlısı
+Profiterol
+Burma Kadayıf
+Fırın Sütlaç
+Krem Karamel
+Tel Kadayıf
+Pestil
+Kalburabastı
+Helva
+Keten Helva
+Düğün Tatlısı
+Zerde
+Şambali
+Şekerli Ekmek
+Şambaba
+Şöbiyet
+Ayva Tatlısı
+Pişmaniye
+Kazandibi Helvası
+Keşkül
+Kaymaklı Ekmek Kadayıfı
+Kazandibi Sütlü Nuriye
+Muhallebili Kadayıf
+Köpük Helva
+Şekerli İrmik Tatlısı
+Damla Sakızlı Muhallebi
+Sütlü Nuriye
+Çikolatalı Muhallebi
+Pelte
+Hurma Tatlısı
+İncir Tatlısı
+Tahin Helvası
+Vişneli Muhallebi
+Halka Tatlısı
+Fıstıklı Baklava
+Tel Şehriye Tatlısı
+Hurma İrmik Tatlısı
+Ayva Dolması
+Yufka Tatlısı
+Kestane Şekeri
+Kızarmış Ekmek Tatlısı
+Dövme Helva
+Şekerpare Baklava
+Külbastı Tatlısı
+Şöbiyet Tatlısı
+Tahinli Kadayıf
+Fıstıklı Şekerpare
+Kuru Kaymak Tatlısı
+Fındıklı Sarma Tatlısı
+İncir Uyutması
+Zeytin Şekeri
+Damla Sakızlı Şekerpare
+İçli Tatlı
+Cevizli Kadayıf
+Çikolatalı Truffle
+Kuru Üzüm Tatlısı
